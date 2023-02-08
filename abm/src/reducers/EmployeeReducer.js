efe61533e107c9bdae6194d0ed93a81ceebeb668
julipoltenguerci <112812@tecnicatura.frc.utn.edu.ar>
2023-02-07 22:08:01 -0300

    Se modifica EmployeeReducer
    Se agrega al array inicialState tres obj empleados y se modifican los casos del reducer según cada acción(agregar, editar y remover).
    Se instala libreria uuid que genera ID aleatorios para cada empleado.

--- abm/src/reducers/EmployeeReducer.js
+++ abm/src/reducers/EmployeeReducer.js
@@ -1,19 +1,74 @@
-import * as actions from "../actions/ActionsType";
+import {
+  ADD_EMPLOYEE,
+  EDIT_EMPLOYEE,
+  REMOVE_EMPLOYEE,
+} from "../actions/EmployeeActions";
 
-export const initialState = {
-  employees: [],
+const initialState = {
+  employees: [
+    {
+      employee_id: "4e451e26-9328-4df5-9f64-5689c3762da3",
+      first_name: "Julieta",
+      last_name: "Polten Guerci",
+      email: "julieta.polten@vortex-it.com",
+      phone_number: 3512436997,
+      hire_date: "01/02/2023",
+      salary: 170000,
+      commission_pct: 0,
+    },
+    {
+      employee_id: "882ab9a0-d735-485a-9d16-e1ad47b7a0e2",
+      first_name: "Luciano",
+      last_name: "Soria",
+      email: "luciano.soria@vortex-it.com",
+      phone_number: 351222333,
+      hire_date: "01/04/2021",
+      salary: 215000,
+      commission_pct: 0,
+    },
+    {
+      employee_id: "eeabdc27-fe22-4a3c-800f-aba160c07e40",
+      first_name: "Migue Angel",
+      last_name: "Medina",
+      email: "miguel.medina@vortex-it.com",
+      phone_number: 3514445555,
+      hire_date: "01/07/2022",
+      salary: 190000,
+      commission_pct: 0,
+    },
+  ],
 };
 
-const EmployeeReducer = (state = initialState, action) => {
+export const employeeReducer = (state = initialState, action) => {
   switch (action.type) {
-    case actions.ADD_EMPLOYEE:
+    case ADD_EMPLOYEE:
       return {
         ...state,
-        employees: [...state.employees, action.payload],
+        employees: [
+          ...state.employees,
+          {
+            ...action.payload,
+            employee_id: uuidv4(),
+          },
+        ],
+      };
+    case EDIT_EMPLOYEE:
+      return {
+        ...state,
+        employees: state.employees.map((employee) =>
+          action.payload.employee_id === employee.employee_id
+            ? action.payload
+            : employee
+        ),
+      };
+    case REMOVE_EMPLOYEE:
+      return {
+        ...state,
+        employees: state.employees.filter(
+          (employee) => action.payload.employee_id !== employee.employee_id
+        ),
       };
     default:
       return state;
   }
 };
-
-export default EmployeeReducer;
